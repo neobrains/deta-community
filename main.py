0edc1ade5be73bc4490a0dbceb80edc7096baf53
LemonPi314 <49930425+LemonPi314@users.noreply.github.com>
2022-09-26 17:10:02 -0400

    Remove jinja2 from main.py because it was not needed

--- main.py
+++ main.py
@@ -2,29 +2,24 @@ import os
 
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import FileResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-from jinja2 import TemplateNotFound
 
-# May separate this into a separate file (routers) if it gets big.
 app = FastAPI()
-app.mount("/assets", StaticFiles(directory="site/assets"), name="assets")
-templates = Jinja2Templates(directory="site")
 
 
 # Try to handle all paths.
 @app.get("/{path:path}")
 async def path(request: Request, path: str):
-    # If the path is a file.
+    # If the path is a file, the path will be relative to '/site'.
     if os.path.splitext(path)[1]:
-        return FileResponse(os.path.join("site", path))
-    # Otherwise, try serving an HTML template with the name.
-    try:
-        return templates.TemplateResponse(os.path.join(path, "index.html"), {"request": request})
-    except TemplateNotFound:
+        path = os.path.join("site", path)
+    # Otherwise, try serving an HTML file with the name.
+    else:
+        path = os.path.join("site", path, "index.html")
+    if not os.path.exists(path):
         raise HTTPException(status_code=404)
+    return FileResponse(path)
 
 
 @app.exception_handler(404)
-async def not_found(request: Request, exc):
-    return templates.TemplateResponse("404.html", {"request": request})
+async def not_found_handler(request: Request, exception):
+    return FileResponse("site/404.html")
