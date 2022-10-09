32dc265ae51a06dc94ba729c1fef8a715de865cd
LemonPi314 <49930425+LemonPi314@users.noreply.github.com>
2022-10-08 20:18:23 -0400

    Add redirects for moved pages

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 
 from fastapi import FastAPI, HTTPException, Request
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, RedirectResponse
 
 app = FastAPI()
 
@@ -9,7 +9,7 @@ app = FastAPI()
 # Try to handle all paths.
 @app.get("/{path:path}")
 async def path(request: Request, path: str):
-    # If the path is a file, the path will be relative to '/site'.
+    # If the path is a file, the path will be relative to "/site".
     if os.path.splitext(path)[1]:
         path = os.path.join("site", path)
     # Otherwise, try serving an HTML file with the name.
@@ -22,4 +22,11 @@ async def path(request: Request, path: str):
 
 @app.exception_handler(404)
 async def not_found_handler(request: Request, exception):
+    # Redirects for moved pages.
+    path = request.url.path.split("/")
+    if path and path[1] == "getting-started":
+        if len(path) > 2 and path[2] == "general":
+            return RedirectResponse("/tutorials")
+        return RedirectResponse(f"/tutorials/{'/'.join(request.url.path.split('/')[2:])}")
+
     return FileResponse("site/404.html")
